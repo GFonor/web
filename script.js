aaabf1d1c65042b26a2409c4da1511ad3de5183f
Fonor <inedelin2005@gmail.com>
2023-10-07 13:03:30 +0300

    ?

--- script.js
+++
@@ -1,6 +0,0 @@
-const navBurger = document.getElementById("nav-burger")
-const navBurgerSlide = document.getElementById("nav-burger-slide")
-navBurger.onclick = function() {
-    console.log("hi")
-    navBurgerSlide.classList.toggle("burger-slide")
-}
